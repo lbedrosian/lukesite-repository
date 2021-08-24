8e90b054cd138dcd94fe0f40d9439fcba9a3c5cd
Luke Bedrosian <bedrosian.luke05@gmail.com>
2021-08-24 05:45:52 -0400

    updated files

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+function changeBackground() {
+ 
+    var text = document.getElementById("joy").style.backgroundColor = "yellow";
+ 
+ 
+}
+ 
+function backToNormal() {
+ 
+    var text = document.getElementById("joy").style.backgroundColor = "";
+ 
+}
 
-document.querySelector("body").appendChild("h2");
+
+    function changeBackground() {
+ 
+        var text = document.getElementById("anger").style.backgroundColor = "red";
+     
+     
+    }
+     
+    function backToNormal() {
+     
+        var text = document.getElementById("anger").style.backgroundColor = "";
+     
+     
+}
+
+function welcome() {
+    alert("Welcome to My WebSite! This is my first time make a project of this size. Reach out to me for any questions! <3")
+}
